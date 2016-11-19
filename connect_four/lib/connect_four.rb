fb0b4b10962d8d68e283c58acaf0452e0c2105ba
Alexander Weitz <ajveits@gmail.com>
2016-11-19 16:20:26 +0200

    added tests for previous projects and fixed connect four

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -1,11 +1,11 @@
-require 'game'
+require_relative 'game'
 
 game = Game.build
 
 
 while game.state == Game::NOT_FINISHED do
 	puts "pick a number between 1 and 7"
-	game.insert(gets.chomp)
+	game.insert(gets.chomp.to_i-1)
 	puts game.to_s
 end
 puts game.state
